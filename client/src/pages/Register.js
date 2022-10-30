df0de88a6e239edb841e0306657c471b5773500e
Jake TyCyn <jaketycyn@gmail.com>
2022-10-30 10:53:36 -0700

    Test Push

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -63,11 +63,6 @@ const Register = () => {
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mt-20 ">
       <div className="w-full max-w-md space-y-8">
         <div>
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-            alt="Workflow"
-          />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             {values.isMember
               ? "Sign in to your account"
